98a91cc0206c454f1b0c039b5ffc5be84a25acfe
Jacob Currie <jcurrie42@my.bcit.ca>
2023-04-09 16:23:45 -0700

    t

--- models/unicorns.js
+++ models/unicorns.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const unicornSchema = new mongoose.Schema({
-    name: String,
-    dob: Date,
-    loves: [String],
-    weight: Number,
-    gender: String,
-    vampires: Number
-  });
+  name: String,
+  dob: Date,
+  loves: [String],
+  weight: Number,
+  gender: String,
+  vampires: Number,
+});
 
 const unicornModel = mongoose.model('unicorns', unicornSchema);
 
--- server.js
+++ server.js
@@ -4,17 +4,16 @@ const mongoose = require('mongoose');
 
 app.use(cors());
 
-// Allow any origin to access the server's resources
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   next();
 });
 
-main().catch(err => console.log(err));
+main().catch((err) => console.log(err));
 
 async function main() {
-  // await mongoose.connect('mongodb://localhost:27017/test');
-  await mongoose.connect('mongodb+srv://jakecurrie:MwvSjEvN3BcCNnYJ@cluster0.7yaerw4.mongodb.net/?retryWrites=true&w=majority');
+  // Change the connection string to point to the 'unicorns' database instead of the default one
+  await mongoose.connect('mongodb+srv://jakecurrie:MwvSjEvN3BcCNnYJ@cluster0.7yaerw4.mongodb.net/unicorns?retryWrites=true&w=majority');
 
   app.listen(3000, () => {
     console.log('Server is running on port 3000');
